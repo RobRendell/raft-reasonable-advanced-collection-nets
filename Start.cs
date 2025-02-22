2c32d27cfe046befb8d5bd232529060119cb313f
Rob Rendell <rob.rendell.au@gmail.com>
2025-02-22 11:24:00 +1100

    Initial commit

---
+++ Start.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class ReasonableAdvancedCollectionNets : Mod
+{
+    public void Start()
+    {
+		var netItem = ItemManager.GetItemByName("Placeable_CollectionNet_Advanced");
+		
+        netItem.settings_recipe.NewCost = new CostMultiple[] { 
+			new CostMultiple(new Item_Base[] { ItemManager.GetItemByName("Plank") }, 9),
+			new CostMultiple(new Item_Base[] { ItemManager.GetItemByName("Rope") }, 10),
+			new CostMultiple(new Item_Base[] { ItemManager.GetItemByName("MetalIngot") }, 3),
+		};
+
+        Debug.Log("ReasonableAdvancedCollectionNets: Changed recipe for Advanced Collection Nets.");
+    }
+
+}
